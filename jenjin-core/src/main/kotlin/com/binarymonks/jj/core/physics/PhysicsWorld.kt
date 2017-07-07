46a7ec2bb2bf55a3e7cb394206d1c3a39556833a
laurencegw <lgwillmore@gmail.com>
2017-07-07 23:23:09 +0100

    Actually it might return null

--- jenjin-core/src/main/kotlin/com/binarymonks/jj/core/physics/PhysicsWorld.kt
+++ jenjin-core/src/main/kotlin/com/binarymonks/jj/core/physics/PhysicsWorld.kt
@@ -1,5 +1,6 @@
 package com.binarymonks.jj.core.physics
 
+import com.badlogic.gdx.math.Vector2
 import com.badlogic.gdx.physics.box2d.Joint
 import com.badlogic.gdx.physics.box2d.JointDef
 import com.badlogic.gdx.physics.box2d.World
